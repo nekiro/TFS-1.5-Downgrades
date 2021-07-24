54ed04d3aaae2945c7ed70b8e562b2e1772c5836
Nekiro <nekiroboy@gmail.com>
2021-07-24 16:55:06 +0200

    Add downgraded message

--- src/otserv.cpp
+++ src/otserv.cpp
@@ -140,6 +140,7 @@ void printServerVersion()
 	std::cout << std::endl;
 
 	std::cout << "A server developed by " << STATUS_SERVER_DEVELOPERS << std::endl;
+	std::cout << "A server downgraded by Nekiro" << std::endl;
 	std::cout << "Visit our forum for updates, support, and resources: https://otland.net/." << std::endl;
 	std::cout << std::endl;
 }
