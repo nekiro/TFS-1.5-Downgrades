e9c5b2bc3d53ab487cf5b2bf48cb052b853ff352
dnvd <88395501+dnvd@users.noreply.github.com>
2021-08-03 23:01:04 +0200

    Fix explosion rune effect (#47)

--- data/spells/scripts/runes/explosion_rune.lua
+++ data/spells/scripts/runes/explosion_rune.lua
@@ -1,7 +1,7 @@
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_EXPLOSIONAREA)
-combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_EXPLOSION)
+combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_FIRE)
 combat:setParameter(COMBAT_PARAM_BLOCKARMOR, true)
 combat:setArea(createCombatArea(AREA_CROSS1X1))
 
