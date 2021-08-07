8d74fea064899b31476485cc47c8764749e6062d
dnvd <dustbin1337@live.se>
2021-08-07 10:36:36 +0200

    fixed energy spell effects

--- data/spells/scripts/attack/force_wave.lua
+++ data/spells/scripts/attack/force_wave.lua
@@ -1,5 +1,5 @@
 local combat = Combat()
-combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_TELEPORT)
 
 combat:setArea(createCombatArea(AREA_SQUAREWAVE5, AREADIAGONAL_SQUAREWAVE5))
