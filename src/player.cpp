db581f3560ab5010b69254eee0edd89ee0d59230
Randomicoanom <randomicoanom@gmail.com>
2021-10-06 08:09:48 -0300

    Follow up PZ lock

--- src/player.cpp
+++ src/player.cpp
@@ -391,6 +391,11 @@ uint16_t Player::getClientIcons() const
 		}
 	}
 
+	if (pzLocked) {
+
+		icons |= ICON_REDSWORDS;
+	}
+
 	if (tile->hasFlag(TILESTATE_PROTECTIONZONE)) {
 		icons |= ICON_PIGEON;
 
