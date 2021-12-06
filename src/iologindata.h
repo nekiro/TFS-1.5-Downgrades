4f6a09d16dac8444d8142f3f8f05d06c31812eaa
Nekiro <nekiro.dev@gmail.com>
2021-12-06 16:39:15 +0100

    Fix depot saving

--- src/iologindata.h
+++ src/iologindata.h
@@ -64,7 +64,6 @@ class IOLoginData
 
 		static void loadItems(ItemMap& itemMap, DBResult_ptr result);
 		static bool saveItems(const Player* player, const ItemBlockList& itemList, DBInsert& query_insert, PropWriteStream& propWriteStream);
-		static bool saveDepotItems(const Player* player, const ItemBlockList& itemList, DBInsert& query_insert, PropWriteStream& propWriteStream);
 };
 
 #endif
