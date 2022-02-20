1245c40543aa7378391a9f61fd6a74e2c59e01ce
Breno Alves <contatobrenoalves@gmail.com>
2022-02-19 22:44:48 -0300

    New function getListOfContainerItems
    - Finds all of the items in a container, and returns it as a table

--- data/global.lua
+++ data/global.lua
@@ -92,6 +92,27 @@ function getLootRandom()
 	return math.random(0, MAX_LOOTCHANCE) / configManager.getNumber(configKeys.RATE_LOOT)
 end
 
+function getListOfContainerItems(container)
+    if not container:isContainer() then
+        return false
+    end
+    
+    local containers, items = {}, {}
+    table.insert(containers, container)
+    while #containers > 0 do
+        for i = 0, containers[1]:getSize() - 1 do  
+            local item = containers[1]:getItem(i)
+            table.insert(items, item)
+            if item:isContainer() then
+                table.insert(containers, item)
+            end
+        end
+        table.remove(containers, 1)
+    end
+    
+    return items
+end
+
 table.contains = function(array, value)
 	for _, targetColumn in pairs(array) do
 		if targetColumn == value then
