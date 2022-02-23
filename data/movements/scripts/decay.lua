9da240679e16d3523255606910e442577fa3f14a
Breno Alves <contatobrenoalves@gmail.com>
2022-02-22 22:11:44 -0300

    Fix script decay.lua for work only for players
    - NPCs was coming down the stairs

--- data/movements/scripts/decay.lua
+++ data/movements/scripts/decay.lua
@@ -1,4 +1,9 @@
 function onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return false
+	end
+	
 	item:transform(item.itemid + 1)
 	item:decay()
 	return true
