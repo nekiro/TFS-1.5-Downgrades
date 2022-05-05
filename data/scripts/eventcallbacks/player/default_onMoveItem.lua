72f7125c58eb15df830d72451e9118035e4e95b2
Luan Luciano <luanluciano@outlook.com>
2022-05-04 23:02:22 -0300

    Update default_onMoveItem.lua

--- data/scripts/eventcallbacks/player/default_onMoveItem.lua
+++ data/scripts/eventcallbacks/player/default_onMoveItem.lua
@@ -2,7 +2,7 @@ local ec = EventCallback
 
 ec.onMoveItem = function(self, item, count, fromPosition, toPosition, fromCylinder, toCylinder)
 	if toPosition.x ~= CONTAINER_POSITION then
-		return true
+		return RETURNVALUE_NOERROR
 	end
 
 	if item:getTopParent() == self and bit.band(toPosition.y, 0x40) == 0 then
@@ -12,22 +12,20 @@ ec.onMoveItem = function(self, item, count, fromPosition, toPosition, fromCylind
 		elseif itemType:getWeaponType() == WEAPON_SHIELD and toPosition.y == CONST_SLOT_RIGHT then
 			moveItem = self:getSlotItem(CONST_SLOT_LEFT)
 			if moveItem and bit.band(ItemType(moveItem:getId()):getSlotPosition(), SLOTP_TWO_HAND) == 0 then
-				return true
+				return RETURNVALUE_NOERROR
 			end
 		end
 
 		if moveItem then
 			local parent = item:getParent()
 			if parent:isContainer() and parent:getSize() == parent:getCapacity() then
-				self:sendTextMessage(MESSAGE_STATUS_SMALL, Game.getReturnMessage(RETURNVALUE_CONTAINERNOTENOUGHROOM))
-				return false
-			else
-				return moveItem:moveTo(parent)
+				return RETURNVALUE_CONTAINERNOTENOUGHROOM
 			end
+			return moveItem:moveTo(parent) and RETURNVALUE_NOERROR or RETURNVALUE_NOTPOSSIBLE
 		end
 	end
 
-	return true
+	return RETURNVALUE_NOERROR
 end
 
 ec:register()
