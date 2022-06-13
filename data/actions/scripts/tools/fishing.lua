232c64cf7e40a8a3f4f4e866673d353370d903a8
Evil Puncker <EPuncker@users.noreply.github.com>
2022-06-13 18:34:29 -0300

    Update fishing.lua
    removed stuff that doesn't belong to 7.x

--- data/actions/scripts/tools/fishing.lua
+++ data/actions/scripts/tools/fishing.lua
@@ -1,8 +1,4 @@
-local waterIds = {493, 4608, 4609, 4610, 4611, 4612, 4613, 4614, 4615, 4616, 4617, 4618, 4619, 4620, 4621, 4622, 4623, 4624, 4625, 7236, 10499, 15401, 15402}
-local lootTrash = {2234, 2238, 2376, 2509, 2667}
-local lootCommon = {2152, 2167, 2168, 2669, 7588, 7589}
-local lootRare = {2143, 2146, 2149, 7158, 7159}
-local lootVeryRare = {7632, 7633, 10220}
+local waterIds = {493, 4608, 4609, 4610, 4611, 4612, 4613, 4614, 4615, 4616, 4617, 4618, 4619, 4620, 4621, 4622, 4623, 4624, 4625}
 local useWorms = true
 
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
@@ -11,34 +7,9 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		return false
 	end
 
-	if targetId == 10499 then
-		local owner = target:getAttribute(ITEM_ATTRIBUTE_CORPSEOWNER)
-		if owner ~= 0 and owner ~= player:getId() then
-			player:sendTextMessage(MESSAGE_STATUS_SMALL, "You are not the owner.")
-			return true
-		end
+	toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
 
-		toPosition:sendMagicEffect(CONST_ME_WATERSPLASH)
-		target:remove()
-
-		local rareChance = math.random(1, 100)
-		if rareChance == 1 then
-			player:addItem(lootVeryRare[math.random(#lootVeryRare)], 1)
-		elseif rareChance <= 3 then
-			player:addItem(lootRare[math.random(#lootRare)], 1)
-		elseif rareChance <= 10 then
-			player:addItem(lootCommon[math.random(#lootCommon)], 1)
-		else
-			player:addItem(lootTrash[math.random(#lootTrash)], 1)
-		end
-		return true
-	end
-
-	if targetId ~= 7236 then
-		toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
-	end
-
-	if targetId == 493 or targetId == 15402 then
+	if targetId == 493 then
 		return true
 	end
 
@@ -48,30 +19,6 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 			return true
 		end
 
-		if targetId == 15401 then
-			target:transform(targetId + 1)
-			target:decay()
-
-			if math.random(1, 100) >= 97 then
-				player:addItem(15405, 1)
-				return true
-			end
-		elseif targetId == 7236 then
-			target:transform(targetId + 1)
-			target:decay()
-
-			local rareChance = math.random(1, 100)
-			if rareChance == 1 then
-				player:addItem(7158, 1)
-				return true
-			elseif rareChance <= 4 then
-				player:addItem(2669, 1)
-				return true
-			elseif rareChance <= 10 then
-				player:addItem(7159, 1)
-				return true
-			end
-		end
 		player:addItem(2267, 1)
 	end
 	return true
