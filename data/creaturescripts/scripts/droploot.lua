3bba794e3f0858439f359c205d898ed22f0cf5fc
Evil Puncker <EPuncker@users.noreply.github.com>
2022-06-13 18:36:24 -0300

    Update droploot.lua

--- data/creaturescripts/scripts/droploot.lua
+++ data/creaturescripts/scripts/droploot.lua
@@ -6,25 +6,11 @@ function onDeath(player, corpse, killer, mostDamageKiller, lastHitUnjustified, m
 	local amulet = player:getSlotItem(CONST_SLOT_NECKLACE)
 	local isRedSkull = player:getSkull() == SKULL_RED
 	if amulet and amulet.itemid == ITEM_AMULETOFLOSS and not isRedSkull then
-		local isPlayer = false
-		if killer then
-			if killer:isPlayer() then
-				isPlayer = true
-			else
-				local master = killer:getMaster()
-				if master and master:isPlayer() then
-					isPlayer = true
-				end
-			end
-		end
-
-		if not isPlayer or not player:hasBlessing(5) then
-			player:removeItem(ITEM_AMULETOFLOSS, 1, -1, false)
-		end
+		player:removeItem(ITEM_AMULETOFLOSS, 1, -1, false)
 	else
 		for i = CONST_SLOT_HEAD, CONST_SLOT_AMMO do
 			local item = player:getSlotItem(i)
-			local lossPercent = player:getLossPercent()
+			local lossPercent = 100
 			if item then
 				if isRedSkull or math.random(item:isContainer() and 100 or 1000) <= lossPercent then
 					if (isRedSkull or lossPercent ~= 0) and not item:moveTo(corpse) then
