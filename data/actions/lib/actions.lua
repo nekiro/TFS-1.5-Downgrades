78440cbb22f44975b0cb5e5266510b2e4cce55ce
Evil Puncker <EPuncker@users.noreply.github.com>
2022-06-13 19:09:04 -0300

    Updated actions

--- data/actions/lib/actions.lua
+++ data/actions/lib/actions.lua
@@ -1,20 +1,15 @@
-local wildGrowth = {1499, 11099} -- wild growth destroyable by machete
+local wildGrowth = {1499} -- wild growth destroyable by machete
 local jungleGrass = { -- grass destroyable by machete
 	[2782] = 2781,
-	[3985] = 3984,
-	[19433] = 19431
+	[3985] = 3984
 }
 local groundIds = {354, 355} -- pick usable ground
-local sandIds = {231, 9059} -- desert sand
+local sandIds = {231} -- desert sand
 local holeId = { -- usable rope holes, for rope spots see global.lua
 	294, 369, 370, 383, 392, 408, 409, 410, 427, 428, 429, 430, 462, 469, 470, 482,
-	484, 485, 489, 924, 1369, 3135, 3136, 4835, 4837, 7933, 7938, 8170, 8249, 8250,
-	8251, 8252, 8254, 8255, 8256, 8276, 8277, 8279, 8281, 8284, 8285, 8286, 8323,
-	8567, 8585, 8595, 8596, 8972, 9606, 9625, 13190, 14461, 19519, 21536, 23713,
-	26020
+	484, 485, 489, 924, 1369, 3135, 3136, 4835, 4837
 }
-local holes = {468, 481, 483, 23712} -- holes opened by shovel
-local fruits = {2673, 2674, 2675, 2676, 2677, 2678, 2679, 2680, 2681, 2682, 2684, 2685, 5097, 8839, 8840, 8841} -- fruits to make decorated cake with knife
+local holes = {468, 481, 483} -- holes opened by shovel
 
 function destroyItem(player, target, toPosition)
 	if type(target) ~= "userdata" or not target:isItem() then
@@ -82,23 +77,6 @@ function onUseMachete(player, item, fromPosition, target, toPosition, isHotkey)
 end
 
 function onUsePick(player, item, fromPosition, target, toPosition, isHotkey)
-	if target.itemid == 11227 then -- shiny stone refining
-		local chance = math.random(1, 100)
-		if chance == 1 then
-			player:addItem(ITEM_CRYSTAL_COIN) -- 1% chance of getting crystal coin
-		elseif chance <= 6 then
-			player:addItem(ITEM_GOLD_COIN) -- 5% chance of getting gold coin
-		elseif chance <= 51 then
-			player:addItem(ITEM_PLATINUM_COIN) -- 45% chance of getting platinum coin
-		else
-			player:addItem(2145) -- 49% chance of getting small diamond
-		end
-		player:addAchievementProgress("Petrologist", 100)
-		target:getPosition():sendMagicEffect(CONST_ME_BLOCKHIT)
-		target:remove(1)
-		return true
-	end
-
 	local tile = Tile(toPosition)
 	if not tile then
 		return false
@@ -116,14 +94,6 @@ function onUsePick(player, item, fromPosition, target, toPosition, isHotkey)
 
 		toPosition.z = toPosition.z + 1
 		tile:relocateTo(toPosition)
-		return true
-	end
-
-	-- Ice fishing hole
-	if ground.itemid == 7200 then
-		ground:transform(7236)
-		ground:decay()
-		toPosition:sendMagicEffect(CONST_ME_HITAREA)
 		return true
 	end
 
@@ -165,7 +135,7 @@ function onUseRope(player, item, fromPosition, target, toPosition, isHotkey)
 			return true
 		end
 
-		if thing:isPlayer() then
+		if thing:isPlayer() or thing:isMonster() then
 			if Tile(toPosition:moveUpstairs()):queryAdd(thing) ~= RETURNVALUE_NOERROR then
 				return false
 			end
@@ -200,26 +170,6 @@ function onUseShovel(player, item, fromPosition, target, toPosition, isHotkey)
 		toPosition.z = toPosition.z + 1
 		tile:relocateTo(toPosition)
 		player:addAchievementProgress("The Undertaker", 500)
-	elseif target.itemid == 7932 then -- large hole
-		target:transform(7933)
-		target:decay()
-		player:addAchievementProgress("The Undertaker", 500)
-	elseif target.itemid == 20230 then -- swamp digging
-		if (player:getStorageValue(PlayerStorageKeys.swampDigging)) <= os.time() then
-			local chance = math.random(100)
-			if chance >= 1 and chance <= 42 then
-				player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You dug up a dead snake.")
-				player:addItem(3077)
-			elseif chance >= 43 and chance <= 79 then
-				player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You dug up a small diamond.")
-				player:addItem(2145)
-			elseif chance >= 80 then
-				player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You dug up a leech.")
-				player:addItem(20138)
-			end
-			player:setStorageValue(PlayerStorageKeys.swampDigging, os.time() + 7 * 24 * 60 * 60)
-			player:getPosition():sendMagicEffect(CONST_ME_GREEN_RINGS)
-		end
 	elseif table.contains(sandIds, groundId) then
 		local randomValue = math.random(1, 100)
 		if target.actionid == actionIds.sandHole and randomValue <= 20 then
@@ -240,10 +190,6 @@ function onUseShovel(player, item, fromPosition, target, toPosition, isHotkey)
 end
 
 function onUseScythe(player, item, fromPosition, target, toPosition, isHotkey)
-	if not table.contains({2550, 10513}, item.itemid) then
-		return false
-	end
-
 	if target.itemid == 2739 then -- wheat
 		target:transform(2737)
 		target:decay()
@@ -251,35 +197,5 @@ function onUseScythe(player, item, fromPosition, target, toPosition, isHotkey)
 		player:addAchievementProgress("Happy Farmer", 200)
 		return true
 	end
-	if target.itemid == 5465 then -- burning sugar cane
-		target:transform(5464)
-		target:decay()
-		Game.createItem(5467, 1, toPosition) -- bunch of sugar cane
-		player:addAchievementProgress("Natural Sweetener", 50)
-		return true
-	end
 	return destroyItem(player, target, toPosition)
-end
-
-function onUseCrowbar(player, item, fromPosition, target, toPosition, isHotkey)
-	if not table.contains({2416, 10515}, item.itemid) then
-		return false
-	end
-
-	return destroyItem(player, target, toPosition)
-end
-
-function onUseKitchenKnife(player, item, fromPosition, target, toPosition, isHotkey)
-	if not table.contains({2566, 10511, 10515}, item.itemid) then
-		return false
-	end
-
-	if table.contains(fruits, target.itemid) and player:removeItem(6278, 1) then
-		target:remove(1)
-		player:addItem(6279, 1)
-		player:getPosition():sendMagicEffect(CONST_ME_MAGIC_GREEN)
-		return true
-	end
-
-	return false
 end
